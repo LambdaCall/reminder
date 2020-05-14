0bd0665dcc383a9fbfa1c43189e47b09eb7b7f54
( <none>
2020-05-14 19:06:22 -0400

    Updates

---
+++ precache-manifest.882867c2827ed272ea2019245f00bc4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef0ab0a5276872c2ddfedc34e6d6cd1",
+    "url": "/reminder/index.html"
+  },
+  {
+    "revision": "53d1a215b750600a126b",
+    "url": "/reminder/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "16633b08ed648e1797d7",
+    "url": "/reminder/static/js/2.60114b41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reminder/static/js/2.60114b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53d1a215b750600a126b",
+    "url": "/reminder/static/js/main.a39265c2.chunk.js"
+  },
+  {
+    "revision": "5b7641d5d7d7e25a141f",
+    "url": "/reminder/static/js/runtime-main.3cd1a476.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reminder/static/media/logo.5d5d9eef.svg"
+  }
+]);
